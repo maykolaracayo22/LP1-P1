0d1ca38eef01f4bb9dcc01226f0653d10506127d
David.Yucra-Mamani <103071814+yucradavid@users.noreply.github.com>
2023-07-06 17:31:14 -0500

    relacione la bd con empleo, realise el bd de crear cuenta, realise el index con bd
    relacione la bd con empleo, realice el bd de crear cuenta, realice el index con bd

--- appBolsaLaboral/routes/index.js
+++ appBolsaLaboral/routes/index.js
@@ -40,17 +40,12 @@ router.get('/admin/crear_cuenta_emp', function(req, res, next) {
   res.render('crear_cuenta_emp');
 });
 
-router.get('/candidatos', function(req, res, next) {
-  res.render('candidatos');
-});
+
 
 router.get('/info', function(req, res, next) {
   res.render('info');
 });
 
-router.get('/empleos', function(req, res, next) {
-  res.render('empleos');
-});
 
 router.get('/contacto', function(req, res, next) {
   res.render('contacto');
@@ -142,6 +137,68 @@ router.get('/admin/logout',function(req, res){
   res.redirect("/");
 });
 
+//---candi
+router.get('/candidatos', function(req, res, next) {
+  dbConn.query('SELECT * FROM egresado ORDER BY egs_id desc',function(err,rows){
+    if(err) {
+        req.flash('error', err);
+        res.render('candidatos',{data:'[]'});   
+    }else {
+        res.render('candidatos',{data:rows});
+    }
+});
+});
 
+
+//------empleos
+
+router.get('/empleos', function(req, res, next) {
+  dbConn.query('SELECT * FROM oferta_laboral ORDER BY ol_id desc',function(err,rows){
+    if(err) {
+        req.flash('error', err);
+        res.render('empleos',{data:'[]'});   
+    }else {
+        res.render('empleos',{data:rows});
+    }
+});
+});
+
+
+//--rout d eimg
+
+router.post('/crear_cuenta_egs', function(req, res, next) { 
+
+  let egs_nombre = req.body.egs_nombre;
+  let egs_ap_paterno = req.body.egs_ap_paterno;
+  let egs_ap_materno = req.body.egs_ap_materno;
+  let egs_dni = req.body.egs_dni;
+  let egs_correo = req.body.egs_correo;
+  let egs_fecha_nacimiento = req.body.egs_fecha_nacimiento;
+
+
+  //console.log(nombre);
+
+      var form_data = {
+        egs_nombre: egs_nombre,
+        egs_ap_paterno: egs_ap_paterno,
+        egs_ap_materno: egs_ap_materno,
+        egs_dni: egs_dni,
+        egs_correo: egs_correo,
+        egs_fecha_nacimiento: egs_fecha_nacimiento
+
+      }
+      
+      // insert query
+      dbConn.query('INSERT INTO egresado SET ?', form_data, function(err, result) {
+          //if(err) throw err
+          if (err) {
+              req.flash('error', err)
+          } else {                
+              req.flash('success', 'egresado registrada satisfactoriamente');
+              res.redirect('egresado');
+          }
+      })
+
+});
 
 module.exports = router;
